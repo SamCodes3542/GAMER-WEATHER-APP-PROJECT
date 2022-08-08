03a2461d8634f1d48dd2b4300169236fcddafdd9
SamCodes3542 <106327181+SamCodes3542@users.noreply.github.com>
2022-08-08 04:00:46 -0500

    added forecast api changed font color to forecast

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const text = document.querySelector(`.text, p`);
-text.innerHTML = text.innerText
-  .split(``)
-  .map(
-    (char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
-  )
-  .join(``);
-
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -32,28 +24,52 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
+
   let days = ["Sun", "Mon", "Tue", "Wed", "Thur"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
               <div class="card">
                 <div class="card-body"> 
-                  ${day}
-                 <br>
-                  81° 
+                  <div class="forecast-day">${formatDay(forecastDay.dt)}
+                 </div>                  
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt="" 
+              width="42"
+               />
                   <br>
-                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun" viewBox="0 0 16 16">
-                    <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-                 </svg>
+              <div class="weather-temps">
+                 <span class="min-temp">${Math.round(
+                   forecastDay.temp.min
+                 )}°</span>
+                 <span class="max-temp">|${Math.round(
+                   forecastDay.temp.max
+                 )}°</span>
+                 </div>
                 </div>
               </div>
             </div>`;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -91,6 +107,16 @@ function displayWeatherConditions(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+
+  const text = document.querySelector(`.text, p`);
+  text.innerHTML = text.innerText
+    .split(``)
+    .map(
+      (char, i) =>
+        `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
+    )
+    .join(``);
   getForecast(response.data.coord);
 }
 
