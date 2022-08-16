1e64746d7beeb589fc4bada6d5ea396d16451406
SamCodes3542 <106327181+SamCodes3542@users.noreply.github.com>
2022-08-16 12:33:01 -0500

    changed index

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function displayForecast(response) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thur"];
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 7) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
